2a304fed2133f823ba19f2df8efac0a9b832162a
Maxim <bmw_077@mail.ru>
2022-08-06 21:11:47 +0300

    category show

--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -26,12 +26,16 @@
                 <!-- Small boxes (Stat box) -->
                 <div class="row">
                     <div class="col-12">
-                    <form action="#" class="w-25">
-                        <div class="form-group">
-                            <input type="text" class="form-control" placeholder="Название категории">
-                        </div>
-                        <input type="submit" class="btn btn-primary" value="Добавить">
-                    </form>
+                        <form action="{{ route ('admin.category.store') }}" method="POST" class="w-25">
+                            @csrf
+                            <div class="form-group">
+                                <input type="text" class="form-control" name="title" placeholder="Название категории">
+                                @error('title')
+                                <div class="text-danger">Это поле необходимо для заполнения</div>
+                                @enderror
+                            </div>
+                            <input type="submit" class="btn btn-primary" value="Добавить">
+                        </form>
                     </div>
                 </div>
                 <!-- /.row -->
