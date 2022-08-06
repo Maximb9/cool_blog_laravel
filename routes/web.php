6060c5f34f03089b59666fd88320ae2fb953c532
Maxim <bmw_077@mail.ru>
2022-08-06 12:22:09 +0300

    create

--- routes/web.php
+++ routes/web.php
@@ -17,14 +17,39 @@ Route::group(['namespace' => 'Main'], function () {
     Route::get('/', 'IndexController');
 });
 
+
 Route::group(['namespace' => 'Admin', 'prefix' => 'admin'], function () {
     Route::group(['namespace' => 'Main'], function () {
         Route::get('/', 'IndexController');
     });
     Route::group(['namespace' => 'Category', 'prefix' => 'categories'], function () {
-        Route::get('/', 'IndexController');
+        Route::get('/', 'IndexController')->name('category.index');
+        Route::get('/create', 'CreateController')->name('category.create');
     });
 });
 
+
 Auth::routes();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
