d3322910495a50674bb738154b8e63b984a4e249
Maxim <bmw_077@mail.ru>
2022-08-11 23:45:40 +0300

    post tags add path two

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -13,6 +13,6 @@ class Post extends Model
     protected $guarded = false;
 
     public function tags() {
-        return $this->belongsToMany(Tag::class, 'post_tags');
+        return $this->belongsToMany(Tag::class, 'post_tags', 'post_id', 'tag_id');
     }
 }
