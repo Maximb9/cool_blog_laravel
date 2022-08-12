6ad0d87e5df067d59d9fb2b7a4626216070c1166
Maxim <bmw_077@mail.ru>
2022-08-13 00:32:56 +0300

    implement an update in posts, load, store images end

--- resources/views/admin/post/edit.blade.php
+++ resources/views/admin/post/edit.blade.php
@@ -26,7 +26,7 @@
                 <!-- Small boxes (Stat box) -->
                 <div class="row">
                     <div class="col-12">
-                        <form action="{{ route('admin.post.update', $post->id) }}" method="POST">
+                        <form action="{{ route('admin.post.update', $post->id) }}" method="POST" enctype="multipart/form-data">
                             @csrf
                             @method('PATCH')
                             <div class="form-group w-25">
@@ -44,8 +44,8 @@
                             </div>
                             <div class="form-group w-50">
                                 <label for="exampleInputFile">Добавить превью</label>
-                                <div class="w-25">
-                                    <img class="w-50" src="{{ $post->preview_image }}" alt="preview_image">
+                                <div class="w-25 mb-2">
+                                    <img class="w-50" src="{{ asset('storage/'. $post->preview_image) }}" alt="preview_image">
                                 </div>
                                 <div class="input-group">
                                     <div class="custom-file">
@@ -62,8 +62,8 @@
                             </div>
                             <div class="form-group w-50">
                                 <label for="exampleInputFile">Добавить главное изображение</label>
-                                <div class="w-25">
-                                    <img class="w-50" src="{{ $post->main_image }}" alt="main_image">
+                                <div class="w-50">
+                                    <img class="w-50 mb-2" src="{{ asset('storage/'. $post->main_image) }}" alt="main_image">
                                 </div>
                                 <div class="input-group">
                                     <div class="custom-file">
@@ -92,7 +92,7 @@
                                 <select class="select2" name="tag_ids[]" multiple="multiple" data-placeholder="<Выберите теги>"
                                         style="width: 100%;">
                                     @foreach($tags as $tag)
-                                        <option {{ is_array( $post->tags) && in_array($tag->id, $post->tags) ? 'selected' : ''}} value="{{ $tag->id }}">{{ $tag->title }}</option>
+                                        <option {{ is_array( $post->tags->pluck('id')->toArray()) && in_array($tag->id, $post->tags->pluck('id')->toArray()) ? 'selected' : ''}} value="{{ $tag->id }}">{{ $tag->title }}</option>
                                     @endforeach
                                 </select>
                             </div>
