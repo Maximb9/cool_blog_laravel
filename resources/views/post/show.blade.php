f97e8e066e91b4367b220cbca1aa28310eefe73a
Maxim <bmw_077@mail.ru>
2022-08-27 18:57:37 +0300

    added related posts

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -19,26 +19,16 @@
             <div class="row">
                 <div class="col-lg-9 mx-auto">
                     <section class="related-posts">
-                        <h2 class="section-title mb-4" data-aos="fade-up">Related Posts</h2>
+                        <h2 class="section-title mb-4" data-aos="fade-up">Схожие посты</h2>
                         <div class="row">
+                            @foreach($relatedPosts as $post)
                             <div class="col-md-4" data-aos="fade-right" data-aos-delay="100">
-                                <img src="assets/images/blog_post_related_1.png" alt="related post"
+                                <img src="{{ asset('storage/'. $post->main_image) }}" alt="related post"
                                      class="post-thumbnail">
-                                <p class="post-category">Blog post</p>
-                                <h5 class="post-title">Front becomes an official Instagram</h5>
+                                <p class="post-category">{{$post->category->title}}</p>
+                                <a href="{{route('post.show', $post->id)}}" class="post-title">{{ $post->title }}</a>
                             </div>
-                            <div class="col-md-4" data-aos="fade-up" data-aos-delay="100">
-                                <img src="assets/images/blog_post_related_2.png" alt="related post"
-                                     class="post-thumbnail">
-                                <p class="post-category">Blog post</p>
-                                <h5 class="post-title">Front becomes an official Instagram</h5>
-                            </div>
-                            <div class="col-md-4" data-aos="fade-left" data-aos-delay="100">
-                                <img src="assets/images/blog_post_related_3.png" alt="related post"
-                                     class="post-thumbnail">
-                                <p class="post-category">Blog post</p>
-                                <h5 class="post-title">Front becomes an official Instagram</h5>
-                            </div>
+                            @endforeach
                         </div>
                     </section>
                     <section class="comment-section">
