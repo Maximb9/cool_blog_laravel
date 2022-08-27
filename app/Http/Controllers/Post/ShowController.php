49f1bbacc703d58f7a0449fe0eb9011eac125a5c
Maxim <bmw_077@mail.ru>
2022-08-27 16:44:36 +0300

    carbon

---
+++ app/Http/Controllers/Post/ShowController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Post;
+
+use App\Http\Controllers\Controller;
+use App\Models\Post;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use function Doctrine\Common\Cache\Psr6\get;
+
+class ShowController extends Controller
+{
+    public function __invoke(Post $post){
+
+        $date = Carbon::parse($post->created_at);
+
+        return view('post.show', compact('post', 'date'));
+    }
+}
